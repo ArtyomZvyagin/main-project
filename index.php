7e3ab5965e863d0586dfc3187e11b20cf150cae1
AtyomZvyagin <artyomzvyagin174@mail.ru>
2023-04-17 15:17:13 +0500

    create structur oop

--- index.php
+++ index.php
@@ -1,51 +1,20 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Клиент-серверное приложение</title>
-</head>
-<body>
+<?php
+header("Content-Type:text/html; charset=UTF-8");
 
-    <form action="insertStudent.php" method="POST">
-        <input type="text" name="fname" id="fname" placeholder="введите имя" required><br>
-        <input type="text" name="lname" id="lname" placeholder="введите фамилию" required><br>
-        <input type="number" name="age" id="age" placeholder="введите возраст" required><br>
-        <input type="radio" name="gender" id="m" value="m" checked>
-        <label for="m">Мужской</label>
-        <input type="radio" name="gender" id="f" value="f">
-        <label for="f">Женский</label><br>
-        <input type="submit" value="Добавить">
-    </form>
+require_once("api/config.php");
+require_once("api/core.php");
 
-    <?php
-    require_once ("config.php");
-
-    //соединение с БД
-    $connect = new mysqli(HOST, USER, PASSWORD, DB);
-    if($connect->connect_error){
-        exit("Ошибка подключения к БД: ".$connect->connect_error);
+if(file_exists("api/main.php")){ //проверяем существование файла
+    include ("api/main.php"); //то подключаем
+    if (class_exists("Main")){ //если существует класс
+        $obj = new Main; //создаем объект от класса Main
+        $obj->get_body();
+    } 
+    else{
+        exit("<p>Верный класс</p>");
     }
+}
 
-    //установить кодировку
-    $connect->set_charset("utf8");
-
-    //код запроса
-    $sql = "SELECT * FROM `students`";
-
-    //выполнить запрос
-    $result = $connect->query($sql);
-
-    //вывести результаты запроса на страницу
-    while ($row = $result->fetch_assoc()) {
-        echo "<div>
-                $row[lname], $row[fname], $row[gender], $row[age]
-            </div>";
+    else{
+        exit("<p>Неверный путь</p>");
     }
-    
-
-
-    ?>
-</body>
-</html>
